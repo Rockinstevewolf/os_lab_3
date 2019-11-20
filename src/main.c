99abd1a52c6ad663def1ea57ba5013bf6e2f9a63
Stepan Barannikov <SteveWolf1997@gmail.com>
2019-11-20 03:36:51 +0300

    done

--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@
 
 #define ERROR_CREATE_THREAD -11
 #define ERROR_JOIN_THREAD   -12
-#define SUCCESS        0
+#define SUCCESS     1
+#define FAIL    11
 
 typedef struct thread_data{
     char **line;
@@ -16,7 +17,8 @@ typedef struct thread_data{
 
 
 // Функция сортировки нисходящим слиянием
-void mergeSort(char **a, int start, int end){
+/*void mergeSort_out(char **a, int start, int end){
+    printf("CATCHargs: %d, %d\n", start, end);
     if(end - start < 2)
         return;
     if(end - start == 2){
@@ -28,8 +30,8 @@ void mergeSort(char **a, int start, int end){
         }
         return;
     }
-    mergeSort(a, start, start + (end-start)/2);
-    mergeSort(a, start + (end-start)/2, end);
+    mergeSort_out(a, start, start + (end-start)/2);
+    mergeSort_out(a, start + (end-start)/2, end);
     char **b = (char**) malloc((end - start) * sizeof(char*));
     int b1 = start, e1 = start + (end-start)/2, b2 = e1;
     for(int i = 0; i < (end - start); i++){
@@ -46,11 +48,69 @@ void mergeSort(char **a, int start, int end){
     for(int i = start; i < end; ++i){
         a[i] = b[i - start];
     }
-}
+    printf("CATCH\n");
+}*/
 
-void* helloWorld(void *args) {
-    printf("Hello from thread!\n");
-    return SUCCESS;
+void *mergeSort(void *args){
+    thread_data *data = (thread_data*) args;
+    int start = data->start, end = data->end;
+    printf("CATCHargs: %d, %d\n", start, end);
+    int status, status_addr;
+    if(end - start < 2)
+        return (void *) SUCCESS;
+    if(end - start == 2){
+        char *temp;
+        if(strcmp(data->line[start], data->line[start+1]) > 0){
+            temp = data->line[start];
+            data->line[start] = data->line[start + 1];
+            data->line[start + 1] = temp;
+        }
+        return (void *) SUCCESS;
+    }
+    pthread_t thread_l, thread_r;
+    thread_data *data_l, *data_r;
+
+    data_l = data;
+    data_l->start = start;
+    data_l->end = start + (end-start)/2;
+    status = pthread_create(&thread_l, NULL, mergeSort,(void *) data_l);
+    if(status == 0){
+        pthread_join(thread_l,(void **) &status_addr);
+        if(status_addr!=SUCCESS){
+            printf("ERROR");
+            return (void *) FAIL;
+        }
+    }
+
+    data_r = data;
+    data_r->start = start + (end-start)/2;
+    data_r->end = end;
+    status = pthread_create(&thread_r, NULL, mergeSort,(void *) data_r);
+    if (status == 0){
+        pthread_join(thread_r,(void **) &status_addr);
+        if(status_addr!=SUCCESS){
+            printf("ERROR");
+            return (void *) FAIL;
+        }
+    }
+    char **b = (char**) malloc((end - start) * sizeof(char*));
+    int b1 = start, e1 = start + (end - start)/2, b2 = e1;
+    for(int i = 0; i < (end - start); i++){
+        b[i] = (char*) malloc(200 * sizeof(char));
+        if(b1 >= e1 || (b2 < end && (strcmp(data->line[b1],data->line[b2]) > 0 || strcmp(data->line[b1],data->line[b2]) == 0))){
+            b[i] = data->line[b2];
+            b2++;
+        }
+        else{
+            b[i] = data->line[b1];
+            b1++;
+        }
+    }
+    for(int i = start; i < end; ++i){
+        data->line[i] = b[i - start];
+    }
+
+    return (void *) SUCCESS;
 }
 
 
@@ -75,7 +135,6 @@ int main(int argc, char *argv[]){
         printf("Error: you must give only 1 file!\n");
         return 0;
     }
-    pthread_t thread;
     int i, j;
     int n;
     fscanf(datafile, "%d\n", &n);
@@ -87,20 +146,30 @@ int main(int argc, char *argv[]){
         printf("%d) %s", i, line[i]);
         if(feof(datafile)){break;}
     }
+    printf("\n\n");
     /////////////////////////////////////////////////////
 
-    /*thread_data data;
+    thread_data data = {line, 0, 10};
+    pthread_t thread;
     int status, status_addr;
     
-    status = pthread_create(&thread, NULL, helloWorld, NULL);
-    if (status != 0) {
+    status = pthread_create(&thread, NULL, mergeSort, (void *) &data);
+    if(status != 0) {
         printf("main error: can't create thread, status = %d\n", status);
         exit(ERROR_CREATE_THREAD);
-    } printf("Hello from main!\n");*/
+    }
+    else if(status == 0){
+        status = pthread_join(thread, (void **) &status_addr);
+        if(status_addr != SUCCESS){
+            printf("ERROR\n");
+            return 0;
+        }
+    }
+    printf("Hello from main!\n");
 
 
-
-    mergeSort(line, 0, 10);
+    printf("\n\n");
+    //mergeSort_out(line, 0, 10);
     printf("\n\n");
     for (i = 0; i < n; i++){
         printf("%d) %s", i, line[i]);
@@ -109,4 +178,6 @@ int main(int argc, char *argv[]){
         free(line[i]);
     }
     free(line);
+
+
 }
